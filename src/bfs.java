e1b3372ea2b99ef79af2fdb27ed504e76a0da713
vsetia <setia.v@northeastern.edu>
2023-04-15 14:33:14 -0400

    add constructor with values

--- src/bfs.java
+++ src/bfs.java
@@ -6,4 +6,14 @@ public class bfs {
   int depth;
   ArrayList<String> solutionPath;
 
+  public bfs() {
+    this.puzzleState = new int[9];
+    this.depth = 0;
+    this.solutionPath = new ArrayList<>();
+  }
+  public bfs(int[] puzzle, int depth, ArrayList<String> path) {
+    this.puzzleState = puzzle;
+    this.depth = depth;
+    this.solutionPath = path;
+  }
 }
