dd3c9499e5d9f459eb8f5fb79215f41c79aac592
vsetia <setia.v@northeastern.edu>
2023-04-26 19:12:49 -0400

    adding comments

--- src/PQueue.java
+++ src/PQueue.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 public class PQueue {
-
+  //Making queue of HeuristicSearch objects for arraylist
   ArrayList<HeuristicSearch> priorityQueue = new ArrayList<>();
 
+  //Check if a state exists in the queue
   public Boolean contains(HeuristicSearch puzzle) {
     for (HeuristicSearch heuristicSearch : priorityQueue) {
       if (stateToString(heuristicSearch.getState()).equals(stateToString(puzzle.getState())))
@@ -12,21 +13,24 @@ public class PQueue {
     return false;
   }
 
+  //Is the queue empty?
   public Boolean isEmpty() {
     return priorityQueue.isEmpty();
   }
 
+  //Add a state to the queue
   public void add(HeuristicSearch puzzle) {
     priorityQueue.add(puzzle);
   }
 
+  //Remove the minimum value state from queue a.k.a. one with closes to soln as per heuristic
   public HeuristicSearch removeMin() {
 
     HeuristicSearch minPuzzle = priorityQueue.get(0);
     int puzzleF = priorityQueue.get(0).getPriority();
 
     for (int i = 1; i < priorityQueue.size(); i++) {
-      int stateF  = priorityQueue.get(i).getPriority();
+      int stateF = priorityQueue.get(i).getPriority();
       if (stateF < puzzleF) {
         minPuzzle = priorityQueue.get(i);
         puzzleF = stateF;
@@ -36,6 +40,7 @@ public class PQueue {
     return priorityQueue.remove(index);
   }
 
+  //convert a state to string
   public String stateToString(int[] state) {
     StringBuilder newState = new StringBuilder();
     for (int i = 0; i < 9; i++) {
